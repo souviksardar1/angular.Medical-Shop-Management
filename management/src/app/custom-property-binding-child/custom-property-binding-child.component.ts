0d47b69a39028ca7f6d5d6de6bd5e00ce997082f
Souvik Sardar <Souvik_Sardar@epam.com>
2023-07-19 10:50:33 +0530

    viewChild & viewcontent

--- management/src/app/custom-property-binding-child/custom-property-binding-child.component.ts
+++ management/src/app/custom-property-binding-child/custom-property-binding-child.component.ts
@@ -1,10 +1,20 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-custom-property-binding-child',
   templateUrl: './custom-property-binding-child.component.html',
   styleUrls: ['./custom-property-binding-child.component.css']
 })
-export class CustomPropertyBindingChildComponent {
+export class CustomPropertyBindingChildComponent{
+
+data : string = '';
+
 @Input() inputProp : {id: string, name: string, address: string}; //coming here from parent
+
+@ViewChild('viewChild', {static: true}) datausingViewChild : ElementRef;
+
+GetDataViewChild(){
+  this.data =  this.datausingViewChild.nativeElement.value;
+  console.log(this.datausingViewChild.nativeElement.value);
+}
 }
